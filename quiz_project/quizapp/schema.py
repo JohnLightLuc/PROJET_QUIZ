379eb125c6ef235634c5105e1d8b7a2eafc7db52
youzerssif <39623776+youzerssif@users.noreply.github.com>
2019-11-06 08:56:52 +0000

    graphene 80% cote quizapp

--- quiz_project/quizapp/schema.py
+++ quiz_project/quizapp/schema.py
@@ -1,108 +1,135 @@
-# import graphene
+import graphene
 
-# from graphene import relay, ObjectType, Connection, Node, Int
-# from graphene_django import DjangoObjectType
-# from graphene_django.filter import DjangoFilterConnectionField
-# from django_filters import FilterSet, OrderingFilter
-# from django.contrib.auth.models import User
-# from comptesApp.schema import UserType
-
-# from .models import *
-
-# # Graphene will automatically map the Article model's fields onto the ArticleNode.
-# # This is configured in the ArticleNode's Meta class (as you can see below)
-# class ExtendedConnection(Connection):
-#     class Meta:
-#         abstract = True
-
-#     total_count = Int()
-#     edge_count = Int()
-
-#     def resolve_total_count(root, info, **kwargs):
-#         return root.length
-#     def resolve_edge_count(root, info, **kwargs):
-#         return len(root.edges)
+from graphene import relay, ObjectType, Connection, Node, Int
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from django_filters import FilterSet, OrderingFilter
+from django.contrib.auth.models import User
 
 
+from .models import *
 
-# class CategorieNode(DjangoObjectType):
-#     class Meta:
-#         model = Categorie
-#         filter_fields = ['titre', 'articles']
+# Graphene will automatically map the Article model's fields onto the ArticleNode.
+# This is configured in the ArticleNode's Meta class (as you can see below)
+class ExtendedConnection(Connection):
+    class Meta:
+        abstract = True
+
+    total_count = Int()
+    edge_count = Int()
+
+    def resolve_total_count(root, info, **kwargs):
+        return root.length
+    def resolve_edge_count(root, info, **kwargs):
+        return len(root.edges)
+
+
+
+class TimemodelsNode(DjangoObjectType):
+    class Meta:
+        model = Timemodels
+        filter_fields = ['date_add', 'date_update','statut']
    
       
-#         interfaces = (relay.Node, )
-#         connection_class = ExtendedConnection
+        interfaces = (relay.Node, )
+        connection_class = ExtendedConnection
 
-# class TagNode(DjangoObjectType):
-#     class Meta:
-#         model = Tag
-#         # Allow for some more advanced filtering here
-#         filter_fields = {
-#             'nom': ['exact', 'icontains', 'istartswith'],
-#             'tag_article__titre':['exact','icontains', 'istartswith'],
+class LevelNode(DjangoObjectType):
+    class Meta:
+        model = Level
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'nom': ['exact', 'icontains', 'istartswith'],
+            'description':['exact','icontains', 'istartswith'],
             
-#         }
-#         interfaces = (relay.Node, )
+        }
+        interfaces = (relay.Node, )
 
 
-# class ArticleNode(DjangoObjectType):
-#     class Meta:
-#         model = Article
-#         filter_fields = {
-#             'titre': ['exact', 'icontains', 'istartswith'],
-#             'titre_slug': ['exact', 'icontains', 'istartswith'],
-#             'commentaires':['exact'],
-#             'categorie_id': ['exact',],
-#             'categorie_id__titre': ['exact'],
-#         }
-#         order_by = OrderingFilter(
-#             fields=(
-#                 ('date_add','date_add'),
-#                 ('vues','vues'),
-#             )
-#         )
-#         interfaces = (relay.Node, )
-#         connection_class = ExtendedConnection
+class QuizNode(DjangoObjectType):
+    class Meta:
+        model = Quiz
+        filter_fields = {
+            'titre': ['exact', 'icontains', 'istartswith'],
+            'description': ['exact', 'icontains', 'istartswith'],
+            'max_question':['exact'],
+            'correction': ['exact',],
+            'level': ['exact'],
+            'single_tentative': ['exact'],
+            'result_tentative': ['exact'],
+            'pourcentage_requis': ['exact'],
+            'success_text': ['exact', 'icontains', 'istartswith'],
+            'fail_text': ['exact', 'icontains', 'istartswith'],
+
+
+        }
+        order_by = OrderingFilter(
+            fields=(
+                ('date_add','date_add'),
+            )
+        )
+        interfaces = (relay.Node, )
+        connection_class = ExtendedConnection
 
 
 
 
 
 
-# class CommentaireNode(DjangoObjectType):
-#     class Meta:
-#         model = Commentaire
-#         # Allow for some more advanced filtering here
-#         filter_fields = {
-#             'contenu': ['exact', 'icontains', 'istartswith'],
-#             'article_id':['exact',],
-#             'username':['exact',],
+class Image_testNode(DjangoObjectType):
+    class Meta:
+        model = Image_test
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'date_update':['exact',],
+            'date_add':['exact',],
+            'statut':['exact',],
+
+
           
-#         }
-#         interfaces = (relay.Node, )
-#         connection_class = ExtendedConnection
+        }
+        interfaces = (relay.Node, )
+        connection_class = ExtendedConnection
 
 
-# class ReplyNode(DjangoObjectType):
-#     class Meta:
-#         model = Reply
-#         # Allow for some more advanced filtering here
-#         filter_fields = {
-#             'contenu': ['exact', 'icontains', 'istartswith'],
-#             'commentaire_id':['exact',],
-#             'username':['exact',],
+class QuestionNode(DjangoObjectType):
+    class Meta:
+        model = Question
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'contenu': ['exact', 'icontains', 'istartswith'],
+            'quiz':['exact',],
+            'level':['exact',],
+            # 'image_question':['exact',],
+            'description': ['exact', 'icontains', 'istartswith'],
           
-#         }
-#         interfaces = (relay.Node, )
-#         connection_class = ExtendedConnection
+        }
+        interfaces = (relay.Node, )
+        connection_class = ExtendedConnection
 
-# class LikeNode(DjangoObjectType):
-#     class Meta:
-#         model = Like
-#         filter_fields = ['date_add',]
-#         interfaces = (relay.Node, )
-#         connection_class = ExtendedConnection
+class ReponseNode(DjangoObjectType):
+    class Meta:
+        model = Reponse
+        filter_fields = {
+            'reponse': ['exact', 'icontains', 'istartswith'],
+            'score':['exact',],
+            'question':['exact',],
+            # 'image_question':['exact',],
+            'description': ['exact', 'icontains', 'istartswith'],
+          
+        }
+        interfaces = (relay.Node, )
+        connection_class = ExtendedConnection
+
+class ResultatNode(DjangoObjectType):
+    class Meta:
+        model = Resultat
+        filter_fields = {
+            'quiz_id':['exact',],
+            'user_id':['exact',],
+        }
+        interfaces = (relay.Node, )
+        connection_class = ExtendedConnection
 
 
 
@@ -140,25 +167,29 @@
 #         return CreateCommentaire(commentaitre=commentaitre)
    
 
-# class Query(ObjectType):
-#     Categorie = relay.Node.Field(CategorieNode)
-#     all_Categories = DjangoFilterConnectionField(CategorieNode)
+class Query(ObjectType):
+    Timemodels = relay.Node.Field(TimemodelsNode)
+    # all_Timemodels = DjangoFilterConnectionField(TimemodelsNode)
     
-#     Tag = relay.Node.Field(TagNode)
-#     all_Tags = DjangoFilterConnectionField(TagNode)
+    Level = relay.Node.Field(LevelNode)
+    # all_Levels = DjangoFilterConnectionField(LevelNode)
 
-#     Article = relay.Node.Field(ArticleNode)
-#     all_Articles = DjangoFilterConnectionField(ArticleNode)
+    Quiz = relay.Node.Field(QuizNode)
+    # all_Quizs = DjangoFilterConnectionField(QuizNode)
 
-#     Commentaire = relay.Node.Field(CommentaireNode)
-#     all_Commentaires = DjangoFilterConnectionField(CommentaireNode)
+    Image_test = relay.Node.Field(Image_testNode)
+    # all_Image_tests = DjangoFilterConnectionField(Image_testNode)
 
 
-#     Like = relay.Node.Field(LikeNode)
-#     all_Likes = DjangoFilterConnectionField(LikeNode)
+    Question = relay.Node.Field(QuestionNode)
+    # all_Questions = DjangoFilterConnectionField(QuestionNode)
     
-#     Reply = relay.Node.Field(ReplyNode)
-#     all_Reply = DjangoFilterConnectionField(ReplyNode)
+    Reponse = relay.Node.Field(ReponseNode)
+    # all_Reponse = DjangoFilterConnectionField(ReponseNode)
+    
+    Resultat = relay.Node.Field(ResultatNode)
+    # all_Resultat = DjangoFilterConnectionField(ResultatNode)
+
     
 # class Mutation(graphene.ObjectType):
         
