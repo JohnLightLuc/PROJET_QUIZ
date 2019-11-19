542391d6ca87e1c3b985dbc80f99e2b4e64cdf33
toure <lamagniguitoure@gmail.com>
2019-11-19 21:34:11 +0000

    last commit

--- quiz_project/quizapp/urls.py
+++ quiz_project/quizapp/urls.py
@@ -6,13 +6,17 @@ from django.urls import path,include
 from . import views
 
 app_name = 'quizapp'
+
+3
+3
 urlpatterns = [
-    path('', views.courses, name='courses'),
-    path('resultat', views.resultat, name='resultat'),
+    path('', views.level, name='courses'),
     path('quiz/<int:id>/', views.quiz, name='quiz'),
     path('quest/<int:id>/', views.quest, name='quest'),
     path('connect/', views.connect, name='connect'),
     path('suscribe/', views.inscription, name='suscribe'),
     path('logout/', views.deconnexion, name='logout'),
     path('sender/', views.senddata),
+    path('resultat/', views.resultat, name='resultat'),
+    path('resultats/', views.resultats, name='resultats'),
 ]
--- quiz_project/quizapp/views.py
+++ quiz_project/quizapp/views.py
@@ -35,19 +35,25 @@ def connect(request):
     return render(request, 'pages/quiz/connexion.html')
 
 @login_required(login_url='connect/')
-def courses(request):
-    levels = Level.objects.filter(statut= True)
-    quiz = Quiz.objects.filter(statut=True)
-    
+def level(request):
+    levels = Level.objects.filter(statut= True)[:4]
     data={
         'levels':levels,
-        'quiz':quiz,
+       
     }
     return render(request, 'pages/quiz/courses.html',data)
 
 @login_required(login_url='connect/')
 def resultat(request):
-    resut = Resultat.objects.all().order_by('-id')[:1]
+    resut = Resultat.objects.filter(user_id__username = request.user.username).order_by('-id')[:1]
+    data={
+        'resut': resut
+    }
+    return render(request, 'pages/quiz/resultat.html',data)
+
+@login_required(login_url='connect/')
+def resultats(request):
+    resut = Resultat.objects.all().order_by('-id')
     data={
         'resut': resut
     }
@@ -68,13 +74,14 @@ def quest(request,id):
     quiz = Quiz.objects.filter(statut=True)
     totalquest = Quiz.objects.filter(statut=True).count()
     somme = Question.objects.filter(statut=True).aggregate(point = Sum('point'))
-
+    id_quiz = id
     print("totalquest = ", totalquest)
     point_total = somme['point']
     data={
         'quiz': quiz,
         'totalquest':totalquest,
         'point_total': point_total,
+        'quiz_id': id_quiz,
     }
     return render(request, 'pages/quiz/quiz.html', data )
 
@@ -84,8 +91,9 @@ def senddata(request):
     # if user.is_authenticated:
     points = postdata['point']
     taux = postdata['taux']
-    util = User.objects.get(username='john')
-    quiz = Quiz.objects.get(id=1)
+    quiz = postdata['id']
+    util = User.objects.get(username= request.user.username)
+    quiz = Quiz.objects.get(id=quiz)
     try:
         result = Resultat()
         result.quiz_id = quiz
@@ -116,6 +124,7 @@ def inscription(request):
                 user = User(username = username, email= email, password= password)
                 user.save()
                 message = "Vous avez été enregistré avec succes! "
+                return redirect('quizapp:connect')
             except:
                 message = "Erreur d'enregistrement ! "
     else:
